c8ea6edf366718a3e33f4e482f6894259d297152
Jonathan Wenger <jowenger@microsoft.com>
2021-02-15 15:00:28 -0800

    Added chart component and rest API for returning the data.

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
 import loaders from './loaders';
@@ -5,17 +6,31 @@ import loaders from './loaders';
 async function startServer() {
   const app = express();
 
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
   const tickers = await loaders();
-  for (let i = tickers.length -1; i >= tickers.length-30; i--) {
-    console.log(tickers[i]);
-  }
-  //console.log(tickers);
 
   app.use(express.static(path.join(__dirname, '../build')));
 
   //Serve the static files for the web application from the root path
   app.get('/', (_req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  });
+
+  app.get('/ticker/:tickerId', async (req, res) => {
+    const { params, query } = req;
+    const { tickerId } = params;
+    const { startDate, endDate }  = query;
+    if (!startDate || !endDate) {
+      res.statusCode = 400;
+      res.send('Bad Request: StartDate or EndDate was missing or invalid.');
+    } else {
+      const responseTickers = tickers.filter(ticker => {
+        return ticker.date >= startDate && ticker.date <= endDate && ticker.ticker === tickerId;
+      });
+      res.json(responseTickers);
+    }
   });
 
   app.listen(8080);
---
+++ src/Ticker.tsx
@@ -0,0 +1,107 @@
+import { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+
+interface ITickerProps {
+    tickerSymbol: string;
+}
+
+interface ITickerState {
+    tickerData: ITickerData[]
+}
+
+interface ITickerData {
+    date: string;
+    nav: string;
+    KID: string;
+    ticker: string;
+    qualifiedTicker: string;
+    ISIN: string;
+    acmeAssetID: number;
+    adjustedPrice: number;
+}
+
+export class Ticker extends Component<ITickerProps, ITickerState> {
+    constructor(props: ITickerProps) {
+        super(props);
+        this.state = {
+            tickerData: []
+        };
+    }
+
+    public componentDidMount() {
+        fetch('/ticker/LALDX?startDate=2019-12-19&endDate=2021-01-31')
+            .then(response => response.json())
+            .then(data => this.setState({
+                tickerData: data
+            }));
+    }
+
+    public render() {
+        const { tickerSymbol } = this.props;
+        const options = {
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Price'
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    },
+                    type: 'time',
+                    time: {
+                        format: 'MM YYYY',
+                        unit: 'month',
+                        displayFormats: { month: 'MMM YYYY' }
+                    }
+                }]
+            },
+        };
+        
+        const data = this.transformData();
+        return (
+            <>
+                <div className='header'>
+                    <h1 className='title'>{`${tickerSymbol} Price Chart`}</h1>
+                </div>
+                <Line data={data} options={options} width={800} height={400} />
+            </>
+        );
+    }
+
+    private transformData = () => {
+        const { tickerData } = this.state;
+        return {
+            datasets: [
+                {
+                    label: 'Unadjusted Value',
+                    data: tickerData.map(item => {
+                        return {
+                            x: moment(item.date),
+                            y: item.nav
+                        };
+                    }),
+                    fill: false,
+                    backgroundColor: '#0492C2',
+                    borderColor: '#3fe1ff',
+                },
+                {
+                    label: 'Adjusted Value',
+                    data: tickerData.map(item => {
+                        return {
+                            x: moment(item.date),
+                            y: item.adjustedPrice
+                        };
+                    }),
+                    fill: false,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgba(255, 99, 132, 0.2)',
+                },
+            ]
+        };
+    }
+}
